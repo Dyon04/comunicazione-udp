d64d68d636cb3c8095baca88af90564a1996b9d0
Dyon04 <diego.tortoioli30@gmail.com>
2023-02-20 13:10:06 +0100

    creazione client server

---
+++ Server.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Server{
+    public static void main(String[] args){
+
+    }
+}
