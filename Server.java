66709bc2ebbd55f78e2eea138b8fd6a75fd44430
Dyon04 <diego.tortoioli30@gmail.com>
2023-02-20 13:38:49 +0100

    definizione del datagramma

--- Server.java
+++ Server.java
@@ -1,14 +1,19 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.*;
+import java.net.*;
 
 public class Server{
     public static void main(String[] args){
+        DatagramSocket serverSocket = new DatagramSocket(2000);
+        boolean attivo = true;  //per la ripetizione del servizio
+        byte[] bufferIN = new byte[1024]; //buffer spedizione e ricezione
+        byte[] bufferOUT = new byte[1024];
 
+            System.out.println("SERVER attivo");
+            while(attivo)
+
+            DatagramPacket receivePacket = new DatagramPacket(bufferIN, bufferIN.lenght); //definizione del datagramma
+
+            serverSocket.receive(reicevePacket); //attesa della ricezione dato dal client
+            
     }
 }
